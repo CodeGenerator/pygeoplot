138260fa3285423d9d76aabb13fba5845c2a53e2
CodeGenerator <k0der@list.ru>
2018-09-14 11:25:58 +0300

    Fix heatmap disposition

--- pygeoplot/display.py
+++ pygeoplot/display.py
@@ -105,7 +105,7 @@ TEMPLATE_HTML = jinja2.Template("""
     require.config({
         paths: {
             "ymaps": "https://api-maps.yandex.ru/2.1/?lang=ru_RU",
-            "heatmap": "https://dl.dropboxusercontent.com/u/20300574/Heatmap.min"
+            "heatmap": "https://cdn.bootcss.com/heatmap.js/2.0.2/heatmap"
         }
     });
 
